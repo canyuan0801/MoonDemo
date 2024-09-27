e67aac7203eeec8cf7742957cf5e1c666437b9bf
canyuan <canyuan0801@outlook.com>
2024-09-27 10:50:02 +0800

    remove 注释影响SQL语句执行

--- server/common/CreateTable.lua
+++ server/common/CreateTable.lua
@@ -7,7 +7,6 @@ end
 
 return function (addr_db)
     local sql = string.format([[
-        --create userdata table
         create table if not exists userdata (
             uid bigint PRIMARY KEY NOT NULL,
             data text
